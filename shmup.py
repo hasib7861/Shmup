37ded62b0ff4e2409586f3b95d0d2e4f543f53fa
Hasib Ali <hasibali2k04@gmail.com>
2022-03-12 17:51:55 +0000

    Created explosions upon collisions

--- shmup.py
+++ shmup.py
@@ -70,6 +70,8 @@ class Player(pygame.sprite.Sprite):
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
         self.shield = 100
+        self.shoot_delay = 250
+        self.last_shot = pygame.time.get_ticks()
 
     def update(self):
         self.speedx = 0
@@ -78,6 +80,8 @@ class Player(pygame.sprite.Sprite):
             self.speedx = -5
         if keystate[pygame.K_RIGHT]:
             self.speedx = 5
+        if keystate[pygame.K_SPACE]:
+            self.shoot()
         self.rect.x += self.speedx
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
@@ -85,10 +89,13 @@ class Player(pygame.sprite.Sprite):
             self.rect.left = 0
 
     def shoot(self):
-        bullet = Bullet(self.rect.centerx, self.rect.top)
-        all_sprites.add(bullet)
-        bullets.add(bullet)
-        shoot_sound.play()
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.shoot_delay:
+            self.last_shot = now
+            bullet = Bullet(self.rect.centerx, self.rect.top)
+            all_sprites.add(bullet)
+            bullets.add(bullet)
+            shoot_sound.play()
 
 
 class Mob(pygame.sprite.Sprite):
@@ -146,27 +153,67 @@ class Bullet(pygame.sprite.Sprite):
             self.kill()
 
 
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, center, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+        self.image = explosion_anim[self.size][0]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.frame = 0
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 50
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.last_update = now
+            self.frame += 1
+            if self.frame == len(explosion_anim[self.size]):
+                self.kill()
+            else:
+                center = self.rect.center
+                self.image = explosion_anim[self.size][self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.center = center
+
+
 # Load all game graphics
-background = pygame.image.load(path.join(img_dir, "Backgrounds\Back.png")).convert()
+background = pygame.image.load(path.join(img_dir, "Backgrounds/Back.png")).convert()
 background_rect = background.get_rect()
-player_img = pygame.image.load(path.join(img_dir, "PNG\playerShip1_orange.png")).convert()
-meteor_img = pygame.image.load(path.join(img_dir, "PNG\Meteors\meteorBrown_med1.png")).convert()
-bullet_img = pygame.image.load(path.join(img_dir, "PNG\Lasers\laserRed16.png")).convert()
+player_img = pygame.image.load(path.join(img_dir, "PNG/playerShip1_orange.png")).convert()
+meteor_img = pygame.image.load(path.join(img_dir, "PNG/Meteors/meteorBrown_med1.png")).convert()
+bullet_img = pygame.image.load(path.join(img_dir, "PNG/Lasers/laserRed16.png")).convert()
 meteor_images = []
-meteor_list = ['PNG\Meteors\meteorBrown_big1.png', 'PNG\Meteors\meteorBrown_big2.png', 'PNG\Meteors\meteorBrown_med1.png',
-               'PNG\Meteors\meteorBrown_med3.png', 'PNG\Meteors\meteorBrown_small1.png', 'PNG\Meteors\meteorBrown_small2.png',
-               'PNG\Meteors\meteorBrown_tiny1.png']
+meteor_list = ['PNG/Meteors/meteorBrown_big1.png', 'PNG/Meteors/meteorBrown_big2.png',
+               'PNG/Meteors/meteorBrown_med1.png', 'PNG/Meteors/meteorBrown_med3.png',
+               'PNG/Meteors/meteorBrown_small1.png', 'PNG/Meteors/meteorBrown_small2.png',
+               'PNG/Meteors/meteorBrown_tiny1.png']
 
 for img in meteor_list:
     meteor_images.append(pygame.image.load(path.join(img_dir, img)).convert())
+
+explosion_anim = {}
+explosion_anim['lg'] = []
+explosion_anim['sm'] = []
+for i in range(9):
+    filename = 'Explosions_kenney/regularExplosion0{}.png'.format(i)
+    img = pygame.image.load(path.join(img_dir, filename)).convert()
+    img.set_colorkey(BLACK)
+    img_lg = pygame.transform.scale(img, (75, 75))
+    explosion_anim['lg'].append(img_lg)
+    img_sm = pygame.transform.scale(img, (32, 32))
+    explosion_anim['sm'].append(img_lg)
 
 # Load all game sounds
 shoot_sound = pygame.mixer.Sound(path.join(snd_dir, 'pew.wav'))
 expl_sounds = []
 for snd in ['expl1.wav', 'expl2.wav']:
     expl_sounds.append(pygame.mixer.Sound(path.join(snd_dir, snd)))
+    pygame.mixer.Sound(path.join(snd_dir, snd)).set_volume(0.1)
 pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
-pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.set_volume(0.1)
+
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -190,9 +237,6 @@ while running:
         # Check for closing the window
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player.shoot()
 
     # Update
     all_sprites.update()
@@ -202,12 +246,16 @@ while running:
     for hit in hits:
         score += 55 - hit.radius
         random.choice(expl_sounds).play()
+        expl = Explosion(hit.rect.center, 'lg')
+        all_sprites.add(expl)
         newmob()
 
     # Check to see if a mob hit the player
     hits = pygame.sprite.spritecollide(player, mobs, True, pygame.sprite.collide_circle)
     for hit in hits:
         player.shield -= hit.radius * 2
+        expl = Explosion(hit.rect.center, 'sm')
+        all_sprites.add(expl)
         newmob()
         if player.shield <= 0:
             running = False
